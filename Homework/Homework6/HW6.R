972b55a9608fd5c6fab9be3320c58af287c11948
Bingcai Liu <liu.3444.osu@gmail.com>
2015-04-20 17:15:46 -0400

    hw6_q17

--- Homework/Homework6/HW6.R
+++ Homework/Homework6/HW6.R
@@ -1,4 +1,5 @@
 setwd("I:/OSU/2015 Spring/STAT 5302/Homeworks/HW6")
+load("I:/OSU/2015 Spring/STAT 5302/Homeworks/HW6/.RData")
 
 ## question 10
 list.model <- data.frame(model=c("None","A","B","C","AB","AC","BC","ABC"),
@@ -24,3 +25,46 @@ cp
 ## d
 BIC = 28 * log(list.model$RSS/28)+log(28)*(list.model$p+1)
 BIC
+
+
+## question 11
+f1 = ((8100-5980)/1)/(5980/26)
+f1
+f2 = ((5980-5500)/1)/(5500/25)
+f2
+fa = ((8100-6240)/1)/(6240/26)
+fa
+fac = ((6240-5250)/1)/(5250/25)
+fac
+fabc = ((5250-5160)/1)/(5160/24)
+fabc
+
+
+## question 17
+## a
+# Model selection
+q17 = read.csv("ex1217.csv")
+Mortality.lm = lm(Mortality ~ . - CITY, data = q17)
+summary(Mortality.lm)
+
+library(leaps)
+M = regsubsets(Mortality ~ . - CITY, data = q17, method = "exhaustive")
+M.sum = summary(M)
+
+plot(M.sum$cp, xlab = "No. of Para", ylab = "CP", pch = 19)
+plot(M.sum$bic, xlab = "No. of Para", ylab = "BIC", pch = 19)
+M.sum
+
+# reduced model and full model
+Mortality.reduced.lm = lm(Mortality ~ Precip + JanTemp + JulyTemp + Educ + NonWhite + SO2,
+                          data = q17)
+summary(Mortality.reduced.lm)
+
+Mortality.full.lm = lm(Mortality ~ Precip + JanTemp + JulyTemp + Educ + NonWhite + HC + NOX + SO2 ,
+                          data = q17)
+summary(Mortality.full.lm)
+
+anova(Mortality.reduced.lm, Mortality.full.lm)
+
+## b
+step(Mortality.lm, direction = "backward", test = "F")
